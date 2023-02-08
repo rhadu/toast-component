9f7c5900dbedbe28c3370efc328e6a774c97681e
Radu Turtoi <radu.turtoi@gmail.com>
2023-02-08 22:16:22 +0200

    Exercise 1 & Exercise 2

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react"
 
-import Button from '../Button';
+import Button from "../Button"
+import Toast from "../Toast"
 
-import styles from './ToastPlayground.module.css';
+import styles from "./ToastPlayground.module.css"
 
-const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
+const VARIANT_OPTIONS = ["notice", "warning", "success", "error"]
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState("")
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0])
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  function handleClick() {
+    setIsOpen(true)
+  }
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -14,50 +23,64 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      <Toast
+        message={message}
+        variant={variant}
+        isOpen={isOpen}
+        handleDismiss={() => {
+          setIsOpen(false)
+        }}
+      />
+
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{alignSelf: "baseline"}}
           >
-            Message
+            Messages
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((option) => (
+              <label key={option} htmlFor={`variant-${option}`}>
+                <input
+                  id={`variant-${option}`}
+                  type="radio"
+                  name="variant"
+                  value={option}
+                  checked={variant === option}
+                  onChange={(event) => {
+                    setVariant(event.target.value)
+                  }}
+                />
+                {option}
+              </label>
+            ))}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button>Pop Toast!</Button>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            <Button onClick={handleClick}>Pop Toast!</Button>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ToastPlayground;
+export default ToastPlayground
